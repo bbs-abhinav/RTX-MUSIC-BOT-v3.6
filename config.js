a876f351914f51c23890b5fbc61bdf6bd892552c
bbs-abhinav <163326798+bbs-abhinav@users.noreply.github.com>
2024-03-26 19:04:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""],
-  botInvite: "",
-  supportServer: "",
+  ownerID: ["1015182519236304967", "1167712316880605214"],
+  botInvite: "https://discord.gg/bQ7qcuJfzy",
+  supportServer: "https://discord.gg/bQ7qcuJfzy",
   mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
-  status: 'RTXX GG',
+  status: 'Made By Abhianv',
   commandsDir: './commands',
   language: "en",
   embedColor: "00fbff",
@@ -13,7 +13,7 @@ module.exports = {
 
   sponsor: {
     status: true,
-    url: "https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A",
+    url: "https://discord.gg/bQ7qcuJfzy",
   },
 
   voteManager: {
